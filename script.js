95d25677e13675d870dc52e92e5565e995aee784
prakharraj1302 <63489114+prakharraj1302@users.noreply.github.com>
2023-01-31 23:33:01 +0530

    Revert "init commit"
    This reverts commit f0530973b4c4a2580eb9c48bdda1e1df7aa53f57.

--- script.js
+++ script.js
@@ -1,50 +1,124 @@
-function isValidISBN(isbn) {
-  // length must be 10
-  console.log(isbn);
-  let n = isbn.length;
-  if (n != 10) return false;
-
-  // Computing weighted sum of
-  // first 9 digits
-  let sum = 0;
-  for (let i = 0; i < 9; i++) {
-    let digit = isbn[i] - "0";
-
-    if (0 > digit || 9 < digit) return false;
-
-    sum += digit * (10 - i);
-  }
-
-  // Checking last digit.
-  let last = isbn[9];
-  if (last != "X" && (last < "0" || last > "9")) return false;
-
-  // If last digit is 'X', add 10
-  // to sum, else add its value.
-  sum += last == "X" ? 10 : last - "0";
-
-  // Return true if weighted sum
-  // of digits is divisible by 11.
-  return sum % 11 == 0;
-}
-var out = document.getElementById("output");
-
-function validateForm() {
-    let x = document.getElementById('input').value;
-    if (x == "") {
-      alert("empty");
-      return false;
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+  
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+  
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+  
+  
+    /*
+        Indexes within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
+    */
+  
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+  
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+  
+    if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+  
+    if (!board.includes(''))
+        announce(TIE);
     }
-    else{
-        if(isValidISBN(x)){
-            out.innerHTML = "VALID";
-        }else{
-            out.innerHTML = "NOT VALID";
+  
+    const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+  
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
+  
+        return true;
+    };
+  
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
+  
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+  
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
         }
     }
-  }
-
-// let isbn = "007462542X";
-
-// if (isValidISBN(isbn)) document.write("Valid");
-// else document.write("Invalid");
+    
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+  
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+  
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
+    }
+  
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+  
+    resetButton.addEventListener('click', resetBoard);
+  });
